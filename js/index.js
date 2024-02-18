ceb0c1a1c1dacc88a62a1eb19b7dd50062f3b3db
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-02-18 22:21:51 +0600

    seats logic added succesfully

--- js/index.js
+++ js/index.js
@@ -5,20 +5,29 @@ function buyTicket() {
 
 }
 
-let selectedSeat = 1;
-let seatLeft = 7;
+let selectedSeat = 0;
+let seatLeft = 8;
+const maxSeat=4;
 const seats = document.getElementsByClassName('seat');
+const clickSeat = document.getElementById('selected-seat')
+const leftSeat = document.getElementById('left-seats');
 for (const seat of seats) {
     seat.addEventListener('click', function () {
-        seat.classList.add('bg-[#1DD100]');
-        const clickSeat = document.getElementById('selected-seat')
-        const leftSeat = document.getElementById('left-seats');
-        console.log(clickSeat.innerText);
-        clickSeat.innerText = selectedSeat;
-        leftSeat.innerText = seatLeft;
-        selectedSeat++;
-        seatLeft--;
+        if(selectedSeat<maxSeat || seat.classList.contains('bg-[#1DD100]')){
+            if (!seat.classList.contains('bg-[#1DD100]')) {
+                seat.classList.add('bg-[#1DD100]');
+                selectedSeat++;
+            }
+            else {
+                seat.classList.remove('bg-[#1DD100]');
+                selectedSeat--;
+            }
+            clickSeat.innerText=selectedSeat;
+            leftSeat.innerText=seatLeft-selectedSeat;
+        }
         
 
     });
-}
+};
+
+
