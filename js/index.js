c09dbd3b08ca8017cb7ab348e40c627606458354
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-02-18 23:26:17 +0600

    select card append child logic build

--- js/index.js
+++ js/index.js
@@ -8,14 +8,27 @@ function buyTicket() {
 let selectedSeat = 0;
 let seatLeft = 8;
 const maxSeat=4;
+const perTicketPrice=550;
 const seats = document.getElementsByClassName('seat');
 const clickSeat = document.getElementById('selected-seat')
 const leftSeat = document.getElementById('left-seats');
 for (const seat of seats) {
     seat.addEventListener('click', function () {
+        const seatValue=seat.innerText;
+        const titleContainer=document.getElementById('title-container');
+        // console.log(seatValue);
         if(selectedSeat<maxSeat || seat.classList.contains('bg-[#1DD100]')){
             if (!seat.classList.contains('bg-[#1DD100]')) {
                 seat.classList.add('bg-[#1DD100]');
+                const p=document.createElement('p');
+                const h1=document.createElement('h1');
+                const h2=document.createElement('h2');
+                p.innerText=seatValue;
+                h1.innerText="Economoy"
+                h2.innerText=550;
+                titleContainer.appendChild(p);
+                titleContainer.appendChild(h1);
+                titleContainer.appendChild(h2);
                 selectedSeat++;
             }
             else {
