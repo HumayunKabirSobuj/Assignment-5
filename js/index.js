e5d0b457237ff958132e1eaff2df616303b429ba
humayunkabir506034 <humayunkabir506034@gmail.com>
2024-02-19 14:06:19 +0600

    modify

--- js/index.js
+++ js/index.js
@@ -7,42 +7,58 @@ function buyTicket() {
 
 let selectedSeat = 0;
 let seatLeft = 8;
-const maxSeat=4;
-const perTicketPrice=550;
+const maxSeat = 4;
+const perTicketPrice = 550;
 const seats = document.getElementsByClassName('seat');
 const clickSeat = document.getElementById('selected-seat')
 const leftSeat = document.getElementById('left-seats');
+
 for (const seat of seats) {
     seat.addEventListener('click', function () {
-        const seatValue=seat.innerText;
-        const titleContainer=document.getElementById('title-container');
+        const seatValue = seat.innerText;
+        const titleContainer = document.getElementById('title-container');
         // console.log(seatValue);
-        if(selectedSeat<maxSeat || seat.classList.contains('bg-[#1DD100]')){
+
+        const p = document.createElement('p');
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h2');
+        p.innerText = seatValue;
+        h1.innerText = "Economoy"
+        h2.innerText = 550;
+        price = clickSeat.innerText * perTicketPrice + 550; 
+        price2=price-perTicketPrice;
+        if (selectedSeat < maxSeat || seat.classList.contains('bg-[#1DD100]')) {
             if (!seat.classList.contains('bg-[#1DD100]')) {
                 seat.classList.add('bg-[#1DD100]');
-                const p=document.createElement('p');
-                const h1=document.createElement('h1');
-                const h2=document.createElement('h2');
-                
-                p.innerText=seatValue;
-                h1.innerText="Economoy"
-                h2.innerText=550;
+                selectedSeat++;
                 titleContainer.appendChild(p);
                 titleContainer.appendChild(h1);
                 titleContainer.appendChild(h2);
-                
-                selectedSeat++;
+                document.getElementById('total-price').innerText = price.toFixed(2);
+
             }
             else {
+
                 seat.classList.remove('bg-[#1DD100]');
+                
+
+                titleContainer.innerHTML = '';
+
+                clickSeat.innerText = selectedSeat;
+                document.getElementById('total-price').innerText =price-550;
                 selectedSeat--;
+
+
             }
-            clickSeat.innerText=selectedSeat;
-            leftSeat.innerText=seatLeft-selectedSeat;
+            clickSeat.innerText = selectedSeat;
+            leftSeat.innerText = seatLeft - selectedSeat;
         }
-        
+
 
     });
 };
+
+
+
 
 
